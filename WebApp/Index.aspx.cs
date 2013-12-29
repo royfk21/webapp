1c88a43705f41ef3bd8389d89e00ff86a6da3843
Roy <royfk21@gmail.com>
2013-12-29 18:09:04 -0500

    First Commit

---
+++ WebApp/Index.aspx.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using AppLibrary;
+
+namespace WebApp
+{
+    public partial class Index : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            App appName = new App() {Name = "Roy"};
+            lblName.Text = "Hello " + appName.Name;
+        }
+    }
+}
